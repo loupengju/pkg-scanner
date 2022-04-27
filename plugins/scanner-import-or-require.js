093abf7af7d33dd0adcaa9cc9882c6d8b15bc1f2
pengju.lou <pengju.lou@weimob.com>
2022-04-27 21:03:10 +0800

    update some logic

--- plugins/scanner-import-or-require.js
+++ plugins/scanner-import-or-require.js
@@ -1,5 +1,10 @@
 const eventbus = require("../utils/eventbus");
 
+const getFirstValue = (value) => {
+  const [firstValue] = value && value.split ? value.split("/") : [];
+  return firstValue;
+}
+
 module.exports = (babel, ...rest) => {
   const modules = rest[rest.length - 1];
   const depArr = [];
@@ -7,8 +12,12 @@ module.exports = (babel, ...rest) => {
     visitor: {
       ImportDeclaration(path, source) {
         const bd = path.node;
+        const value = getFirstValue(bd.source.value);
         // import
-        if (bd.type === 'ImportDeclaration' && modules.includes(bd.source.value)) {
+        if (bd.type === 'ImportDeclaration' && value && modules.includes(value)) {
+          !depArr.includes(value) && depArr.push(value);
+        }
+        if (bd.type === 'ImportDeclaration' && (modules.includes(bd.source.value))) {
           !depArr.includes(bd.source.value) && depArr.push(bd.source.value);
         }
       },
@@ -19,6 +28,10 @@ module.exports = (babel, ...rest) => {
           bd.declarations.forEach(dec => {
             if (dec.init &&  dec.init.arguments &&  dec.init.arguments.length) {
               dec.init.arguments.forEach(arg => {
+                const value = getFirstValue(arg && arg.value ? arg.value : '');
+                if (value && modules.includes(value)) {
+                  !depArr.includes(value) && depArr.push(value);
+                }
                 if (arg && arg.value && modules.includes(arg.value)) {
                   !depArr.includes(arg.value) && depArr.push(arg.value);
                 }
@@ -29,25 +42,24 @@ module.exports = (babel, ...rest) => {
 
       },
       Literal(path, source) {
+        const value = getFirstValue(path.node.value || '');
+        value && !depArr.includes(value) && depArr.push(value);
         path.node.value && !depArr.includes(path.node.value) && depArr.push(path.node.value);
       },
       CallExpression(path, source) {
         const bd = path.node;
         if (bd.callee && bd.callee.name && bd.callee.name === 'require' && bd.arguments && bd.arguments.length) {
           bd.arguments.forEach(arg => {
+            const value = getFirstValue(arg && arg.value ? arg.value : '');
+            if (value && modules.includes(value)) {
+              !depArr.includes(value) && depArr.push(value);
+            }
             if (arg && arg.value && modules.includes(arg.value)) {
               !depArr.includes(arg.value) && depArr.push(arg.value);
             }
           })
         }
       },
-      MemberExpression(path, source) {
-        const bd = path.node;
-        if (bd.object && bd.object.name && bd.object.name === 'require' && bd.property && bd.property.name && modules.includes(bd.property.name)) {
-          !depArr.includes(bd.property.name) && depArr.push(bd.property.name);
-        }
-      },
-
     },
     post(state) {
       eventbus.$emit('depArr', depArr)
